04665d2074639aded0c2a8d662352393ae3dbff4
Глеб Бушланов <miracle_5@bk.ru>
2022-06-14 17:14:40 +0300

    Done num 6

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,34 +7,50 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=20, unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=20,
+        unique=True,
+        verbose_name='Путь в ссылке'
+    )
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Группа'
     )
 
     class Meta:
-        ordering = ['-pub_date']
-        verbose_name = 'post'
-        verbose_name_plural = 'posts'
+        ordering = (('-pub_date'),)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
     def __str__(self) -> str:
         return self.text[:settings.TEXT_SIZE_NUMBER]
